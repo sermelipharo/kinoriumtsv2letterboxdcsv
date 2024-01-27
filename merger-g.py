9aff40b40062b89ecf67009acfb36eb04d56768c
sermelipharo <sermelipharo@users.noreply.github.com>
2024-01-27 20:05:19 +0400

    Multiple languages support [UA+ENG]
    Added support for dumps in English and Ukrainian, added localization in English

--- merger-g.py
+++ merger-g.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog, messagebox
+import locale
+
+# Установка локали и определение текущей локали пользователя
+locale.setlocale(locale.LC_CTYPE, '')
+current_locale, _ = locale.getlocale()
+
+# Установка текстов интерфейса в зависимости от локали
+if current_locale.startswith(('hy', 'ru', 'be', 'uk')):
+    # Русский язык
+    votes_file_title = "Выберите файл с оценками"
+    comments_file_title = "Выберите файл с комментариями"
+    folder_chooser_title = "Выберите папку для сохранения результатов"
+    success_message = "Файлы успешно обработаны и сохранены!"
+    warning_message = "Не выбраны все необходимые файлы или папка."
+else:
+    # Английский язык
+    votes_file_title = "Select the ratings file"
+    comments_file_title = "Select the comments file"
+    folder_chooser_title = "Select a folder to save the results"
+    success_message = "Files have been successfully processed and saved!"
+    warning_message = "Not all necessary files or folder have been selected."
 
 def select_file(title):
     root = tk.Tk()
@@ -20,25 +41,23 @@ def process_files(votes_file, comments_file, output_path):
     df_comments = pd.read_csv(comments_file, sep='\t', encoding='utf-16le')
 
     # Фильтрация, чтобы оставить только фильмы и мультфильмы
-    df_votes = df_votes[df_votes['Type'].isin(['Фильм', 'Мультфильм'])]
+    film_types = ['Фильм', 'Мультфильм', 'Movie', 'Animation Movie', 'Фільм', 'Мультфільм']
+    df_votes = df_votes[df_votes['Type'].isin(film_types)]
 
-    # Объединение данных без изменения даты
-    merged_df = pd.merge(df_votes, df_comments, on=['Date', 'Original Title', 'Year', 'My rating'], how='left', suffixes=('', '_comment'))
+    # Обработка в зависимости от наличия столбца 'Original Title'
+    if 'Original Title' in df_votes.columns:
+        # Если есть столбец 'Original Title'
+        merged_df = pd.merge(df_votes, df_comments, on=['Date', 'Original Title', 'Year', 'My rating'], how='left', suffixes=('', '_comment'))
+        merged_df['Title'] = merged_df['Original Title'].fillna(merged_df['Title'])
+    else:
+        # Если столбца 'Original Title' нет
+        merged_df = pd.merge(df_votes, df_comments, on=['Date', 'Title', 'Year', 'My rating'], how='left', suffixes=('', '_comment'))
 
-    # Создание копии датафрейма для обработки
-    processed_df = merged_df[['Original Title', 'Title', 'Year', 'My rating', 'Date', 'Comment']].copy()
-
-    # Заполнение 'Original Title' значениями из 'Title', если 'Original Title' пуст
-    processed_df['Title'] = processed_df['Original Title'].fillna(processed_df['Title'])
-
-    # Удаление лишнего столбца 'Original Title'
-    processed_df.drop(columns=['Original Title'], inplace=True)
-
-    # Переименование столбцов и преобразование типов данных
+    # Обработка датафрейма
+    processed_df = merged_df[['Title', 'Year', 'My rating', 'Date', 'Comment']].copy()
+    processed_df.drop(columns=['Original Title'], errors='ignore', inplace=True)
     processed_df.rename(columns={'My rating': 'Rating10', 'Date': 'WatchedDate', 'Comment': 'Review'}, inplace=True)
     processed_df['Rating10'] = processed_df['Rating10'].fillna(0).astype(int)
-
-    # Преобразование формата даты
     processed_df['WatchedDate'] = pd.to_datetime(processed_df['WatchedDate']).dt.strftime('%Y-%m-%d')
 
     # Разделение на части и сохранение каждой части в отдельный файл
@@ -48,13 +67,13 @@ def process_files(votes_file, comments_file, output_path):
         chunk_df.to_csv(f"{output_path}{i+1}.csv", index=False, encoding='utf-8-sig')
 
 # Выбор файлов и папки для сохранения
-votes_file = select_file("Выберите файл с оценками")
-comments_file = select_file("Выберите файл с комментариями")
-output_folder = select_output_folder("Выберите папку для сохранения результатов")
+votes_file = select_file(votes_file_title)
+comments_file = select_file(comments_file_title)
+output_folder = select_output_folder(folder_chooser_title)
 
 if votes_file and comments_file and output_folder:
     output_path = output_folder + '/export_'
     process_files(votes_file, comments_file, output_path)
-    messagebox.showinfo("Готово", "Файлы успешно обработаны и сохранены!")
+    messagebox.showinfo("Done", success_message)
 else:
-    messagebox.showwarning("Внимание", "Не выбраны все необходимые файлы или папка.")
+    messagebox.showwarning("Warning", warning_message)
